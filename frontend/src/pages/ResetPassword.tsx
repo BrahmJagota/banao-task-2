942fcde30fe6e45d9b6e0cdd26c53a5d242941d4
unknown <jagotabrahm@gmail.com>
2025-02-14 22:52:36 +0530

    removed unused

--- frontend/src/pages/ResetPassword.tsx
+++ frontend/src/pages/ResetPassword.tsx
@@ -1,5 +1,5 @@
 import { useForm, SubmitHandler } from "react-hook-form"
-import {User, Lock} from 'lucide-react'
+import {Lock} from 'lucide-react'
 import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 interface FormData {
